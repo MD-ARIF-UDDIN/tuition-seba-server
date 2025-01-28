6f05eb420bf2952d5a7d6a25e75838b19bde428d
MD Arif <leoarifuddin@gmail.com>
2025-01-29 03:18:44 +0600

    p

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.delete('/api/sheets', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
